721b49773c61a95a8629cf51d0ac25b3afef7e3c
Amardeep Saha <amardeep.saha@indiamart.com>
2025-08-08 10:14:58 +0530

    DB model work | user login steps | JWT payload creation logic

--- video-backend/models/vc-user-model.js
+++ video-backend/models/vc-user-model.js
@@ -9,10 +9,13 @@ class UserLogin extends Model{
 UserLogin.init({
 // model attributes
     id:{
+        type:DataTypes.INTEGER,
+        autoIncrement:true,
+        primaryKey:true,
+    },
+    passstring:{
         type:DataTypes.STRING(255),
         allowNull:false,
-        autoIncrement:false,
-        primaryKey:true,
     },
     username:{
         type:DataTypes.STRING(50),
@@ -50,13 +53,20 @@ UserDetails.init({
             type:DataTypes.CHAR(50)
         },
         email:{
-            type:DataTypes.CHAR(100)
+            type:DataTypes.CHAR(100),
+            unique:true,
         },
         mobile:{
-            type:DataTypes.CHAR(11)
+            type:DataTypes.CHAR(11),
+            unique:true,
         },
         userid:{
-            type:DataTypes.STRING(255),
+            type:DataTypes.INTEGER,
+        },
+        idenMode:{
+            type:DataTypes.CHAR(2),
+            allowNull:false,
+            autoIncrement:false,
         }
 },{
     modelName:'vc_user_details',
@@ -128,43 +138,80 @@ async function getUser(userIdentifier, mode){ // mode = LOOKUPLOGIN | LOOKUPDATA
 async function createUser({username , userpass}) {
     try{
         const userPresent = await getUser(username,"LOOKUPLOGIN");
-        if(userPresent.data !== "") return {status : 'failure', message:'user creation failure, the user already exsists', data:''}
-        const userId = createHash('SHA256').update(`${username}-${userpass}`).update(process.env.MYSQL_USERPASS_SALT).digest('base64')
+        if(userPresent.data !== "") throw new Error ('user creation failure, the user already exsists');
         const secretKey = createHash('SHA256').update(username).update(process.env.MYSQL_USERSECRET_SALT).digest('base64')
-        const createUser = await UserLogin.create({
-            'id':userId,
+        const passstring = createHash('SHA256').update(`${username}-${userpass}`).update(process.env.MYSQL_USERPASS_SALT).digest('base64')
+        const createUser = await UserLogin.save({
             'username':username,
+            'passstring':passstrings,
             'secret_key':secretKey,
         })
         
-        let updateUserDetail = UserDetails.build({
-            userid : userId,
+        let userID = await createUser.getDataValue('id');
+        
+        let UserDetail = UserDetails.build({
+            userid : userID,
         })
         
         const emailRegexp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
         const mobileTestUS = /^(\+1[-.\s]?)?\(?([0-9]{3})\)?[-.\s]?([0-9]{3})[-.\s]?([0-9]{4})$/
         const mobileTestIN = /^(\+91[-.\s]?)?[6-9]\d{9}$/
         const mobileTestOthers = /^(\+[1-9]\d{0,3}[-.\s]?)?\d{10,14}$/ 
+        const idenMode = '';
 
         if(emailRegexp.test(`${username}`)){
-            updateUserDetail.email = username
+            UserDetail.email = username;
+            idenMode = 'EM'
         }else if(mobileTestIN.test(`${username}`) || mobileTestOthers.test(`${username}`) || mobileTestUS.test(`${username}`)){
-            updateUserDetail.mobile = username
+            UserDetail.mobile = username;
+            idenMode = 'MB'
         }else{
             throw new Error('invalid Data Type')
         }
 
-        updateUserDetail =  await updateUserDetail.save();
-        console.log(updateUserDetail,createUser);
+        try{
+            UserDetail =  await UserDetail.save();
+            return {status : 'failure', message :"success", data: UserDetail }
+        }catch(err){
+            throw new Error('Unable to update userdetails')
+        }
+        // console.log(updateUserDetail,createUser);
 
     }catch(err){
         return {status : 'failure', message :err.message, data: err.stack } // error here
     }
 }
 
+
+async function loginUser({username , userpass}){
+    const passstring =createHash('SHA256').update(`${username}-${userpass}`).update(process.env.MYSQL_USERPASS_SALT).digest('base64');
+
+    const userSecretKey = await UserLogin.findAll({
+        attributes:[secretKey, id],
+        where:{
+            [Op.and] : [{username},{passstring}]
+        }
+    })
+
+    if(userSecretKey.length  == 0 ){
+        // run the query to fetch the primary mode of identification and return the data to the user
+    }else{
+        const secret_key = userSecretKey[0].getDataValue('secret_key');
+        const id = userSecretKey[0].getDataValue('id');
+        const PayloadForValidJWT = {
+            'user-id':id,
+            'dataString':"here goes encrypted data string" // {passstring + username seperated by a '|' passed through a stream cipher , probably will be using RC4}
+        }
+
+        return {status:'success', message:'userLoginsuccess', data :{JWTpayload : PayloadForValidJWT}}
+    }
+}
+
 // getUser('amardeepsaha13@gmail.com','LOOKUPDATA').then(console.log)
 
-createUser({username:'amardeepsaha13@gmail.vom', password:'AmardeepSaha1234'}).then(console.log).catch(console.error);
+// createUser({username:'amardeepsaha13@gmail.vom', password:'AmardeepSaha1234'}).then(console.log).catch(console.error);
+
+
 
 
 module.exports = { UserLogin, UserDetails}
